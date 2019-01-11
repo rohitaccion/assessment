8a5c1f1e49f59335b6b4342d6696e4fae8a4f30a
core.autocrlf <rohit.batra@minditsystems.com>
2019-01-11 16:09:09 +0530

    Moved from Quiz local to assessment

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,5 +1,5 @@
 <?php 
-		$sq_base_url='http://localhost/quiz/';
+		$sq_base_url='http://localhost/assessment/';
 		$sq_hostname='localhost';
 		$sq_dbname='quiz';
 		$sq_dbusername='root';
---
+++ application/logs/log-2019-01-11.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-01-11 16:07:19 --> Severity: Warning --> Division by zero /Applications/XAMPP/xamppfiles/htdocs/assessment/application/views/dashboard.php 231
+ERROR - 2019-01-11 16:07:39 --> Severity: Warning --> Division by zero /Applications/XAMPP/xamppfiles/htdocs/assessment/application/views/dashboard.php 231
+ERROR - 2019-01-11 16:08:21 --> Severity: Warning --> Division by zero /Applications/XAMPP/xamppfiles/htdocs/assessment/application/views/dashboard.php 231
